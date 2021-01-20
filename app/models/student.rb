43603e4bc1c597f738a977eaf9a0618edf926ad7
David Leonardo <fanger53@gmail.com>
2021-01-20 14:24:16 -0500

    add the model student

---
+++ app/models/student.rb
@@ -0,0 +1,3 @@
+class Student < ApplicationRecord
+  belongs_to :house
+end
---
+++ db/migrate/20210120191929_create_students.rb
@@ -0,0 +1,10 @@
+class CreateStudents < ActiveRecord::Migration[6.1]
+  def change
+    create_table :students do |t|
+      t.string :name
+      t.references :house, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
