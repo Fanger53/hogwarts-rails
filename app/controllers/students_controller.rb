bd9e8db7a6baf90d562a707b6ecc0db9691c4f0e
David Leonardo <fanger53@gmail.com>
2021-01-22 14:50:07 -0500

    implementation of students view index

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,5 +1,6 @@
 class StudentsController < ApplicationController
   def index
+    @students = Student.all
   end
 
   def show
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
-  get 'students/index'
-  get 'students/show'
+  resources 'students', only: %i[index show]
   resources 'houses', only: %i[index show]
   root 'houses#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
